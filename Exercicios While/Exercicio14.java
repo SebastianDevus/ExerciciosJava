0075828084d61301e6d651c3b0a5665145a5001a
Sebastian-Devus <86898507+SebastianDevus@users.noreply.github.com>
2024-08-30 18:20:05 -0300

    Remove comentários contendo código não usado

--- Exercicios While/Exercicio14.java
+++ Exercicios While/Exercicio14.java
@@ -22,11 +22,9 @@ public class Exercicio14 {
 			senha = Integer.parseInt(sc.nextLine());
 			
 			// Se a senha inserida for incorreta, avisa usuário
-			// if (senha > 1000) {
 			if (senha != senhaCorreta) {
 				System.out.println("Senha Incorreta \n");
 			}
-		// } while (senha > 1000);
 		} while (senha != senhaCorreta); // Se a senha for incorreta, volta ao começo do laço
 		
 		// Se a senha for correta, avisa usuário e acaba programa
